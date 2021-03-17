5f6564d193637baba59b3ff8b19a16abb38c83de
Igor Sabadosh <z32.nivelir@gmail.com>
2021-03-17 13:37:58 +0200

    fix reducers

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -4,6 +4,7 @@ export const getInitialState = (numberOfFlights) => {
   const flightList = flightDummyDataGenerator(numberOfFlights);
   return {
     flights: flightList,
+    openModal: undefined,
   };
 };
 
@@ -11,18 +12,20 @@ export const reducer = (state, action) => {
   switch (action.type) {
     case "remove": {
       const newFlights = state.flights.filter(({ id }) => id !== action.id);
-      return {
-        cards: [...newFlights],
-      };
+      return { ...state, flights: [...newFlights] };
     }
     case "edit": {
       const idx = state.flights.findIndex((f) => f.id === action.id);
       const newFlights = [...state.flights];
       newFlights[idx][status] = action.status;
 
-      return {
-        cards: [...newFlights],
-      };
+      return { ...state, flights: [...newFlights] };
+    }
+    case "open": {
+      return { ...state, openModal: action.id };
+    }
+    case "close": {
+      return { ...state, openModal: undefined };
     }
     default:
       return state;
