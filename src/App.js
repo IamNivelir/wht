eac1f985dc2ff029ae0f7a141ee08f38241c80a4
Igor Sabadosh <z32.nivelir@gmail.com>
2021-03-17 12:02:55 +0200

    populate state with an actual data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import StateContext from "./lib/StateContext";
 import ArrivalFlightList from "./pages/ArrivalFlightList";
 
 const App = () => {
-  const initialState = getInitialState();
+  // Lets generate 10 items for the flight list
+  const initialState = getInitialState(10);
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
