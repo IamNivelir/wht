b11df0cf8db3ffd1f23c87ad347ea7c0f6088ee6
Igor Sabadosh <z32.nivelir@gmail.com>
2021-03-17 12:08:24 +0200

    Show actual data from app's state

--- src/pages/ArrivalFlightList.jsx
+++ src/pages/ArrivalFlightList.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import StateContext from "../lib/StateContext";
 import FlightCard from "../components/FlightCard";
 
 const ArrivalFlightList = () => {
+  const {
+    state: { flights },
+  } = useContext(StateContext);
+
+  const getFlightList = () => {
+    return flights
+      .sort((a, b) => {
+        return new Date(a.scheduledArrival) - new Date(b.scheduledArrival);
+      })
+      .map((flightData) => (
+        <FlightCard data={flightData} key={flightData.id} />
+      ));
+  };
+  const flightList = getFlightList();
+
   return (
     <div>
       <h3>List Of Arrivals</h3>
-      <div>
-        <FlightCard />
-      </div>
+      <div>{flightList}</div>
     </div>
   );
 };
